3acdd6e8e3f672a4d348d3a4007f7621f890d883
zxsaqc <735548459@qq.com>
2022-12-04 00:06:00 +0800

    Update fileball-aliyun.js

--- fileball-aliyun.js
+++ fileball-aliyun.js
@@ -1,6 +1,6 @@
 #fileball挂载阿里云
 #qx规则
 
-^http://aliyun\.example\.com url script-analyze-echo-response fileball-aliyun.js
+^http://aliyun\.example\.com url script-analyze-echo-response https://raw.githubusercontent.com/zxsaqc/qx/master/aliyun.js
 
 hostname = aliyun.example.com
