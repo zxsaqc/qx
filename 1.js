b0728ac217e8478a8ccc9bb35ef27cfc39ae4b7f
zxsaqc <735548459@qq.com>
2022-12-17 00:50:17 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,7 @@
+[rewrite_local]
+
+http:\/\/iosv2\.cjapi\.(5fun|wufan88)\.com\/user\/.+ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wfzy.js
+
+[mitm] 
+
+hostname = iosv2.cjapi.wufan88.com,iosv2.cjapi.5fun.com
